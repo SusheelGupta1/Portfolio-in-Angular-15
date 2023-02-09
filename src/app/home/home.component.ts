a128434ec621e13171806983f301073a597ea70f
Susheel Gupta <susheelgupta81086@gmail.com>
2023-02-09 10:09:12 +0530

    portfolio

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  introduction= [
+    {
+      Name:'Hello Im Susheel GuptaLooking for a challenging role in are put able organization to utilize my technical, database and management. Skills for the growth of the organization as to enhance my knowledge about new and emerging trends in the IT sector.',
+    }
+  ]
+}
